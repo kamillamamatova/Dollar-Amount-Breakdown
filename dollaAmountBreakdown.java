5e8ce4409a98497e8a7ffb2c1363b70433aeacb6
Kamilla  Mamatova <kamillamamatova.km@gmail.com>
2025-05-02 23:53:51 -0400

    Create dollaAmountBreakdown.java

---
+++ dollaAmountBreakdown.java
@@ -0,0 +1,6 @@
+import java.util.Scanner;
+
+public class HW1{
+    public static void main(String[] args){
+        double amount;
+        int hundreds, fifties, twenties, tens, fives, ones, quarters, dimes, nickels, cents;

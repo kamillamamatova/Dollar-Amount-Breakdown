49c2c2621fe1e2f933b0f17da4caeca3010c98eb
Kamilla  Mamatova <kamillamamatova.km@gmail.com>
2025-05-02 23:54:28 -0400

    Update dollarAmountBreakdown.java

--- dollarAmountBreakdown.java
+++ dollarAmountBreakdown.java
@@ -4,3 +4,9 @@ public class HW1{
     public static void main(String[] args){
         double amount;
         int hundreds, fifties, twenties, tens, fives, ones, quarters, dimes, nickels, cents;
+
+        Scanner myScanner = new Scanner(System.in);
+
+        // Asks the user to enter a dollar amount
+        System.out.print("Enter your dollar amount: ");
+        amount = myScanner.nextDouble();
